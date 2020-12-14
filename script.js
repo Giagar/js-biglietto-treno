5859586314a4df1e0a395c5a57460959ea940976
Gianpaolo <pao.barga@gmail.com>
2020-12-14 16:11:26 +0100

    Done html and js: everything is working

---
+++ script.js
@@ -0,0 +1,24 @@
+// Variabili generiche
+var result = "";
+
+// Dati raccolti dall'utente
+var km = parseInt(prompt('Quanti km vuoi percorrere?'));
+var age = parseInt(prompt('Quanti anni hai?'));
+
+// Costo in base ai km percorsi 
+var cost = km * 0.21;
+
+// Costo aggiornato in base all'età
+if (age < 18) {
+    cost = cost - (cost / 100 * 20);
+} else if (age > 65) {
+    cost = cost - (cost / 100 * 40);
+} else {
+    null; 
+}
+
+// Costo arrotondato ai centesimi
+cost = Math.round(cost);
+result = "Il costo del biglietto è di " + cost + "€";
+
+document.querySelector("#cost-container").innerHTML = result;
