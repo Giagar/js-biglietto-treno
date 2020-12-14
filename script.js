75fd56cc41d449222b52f31e4832d7b9f2d56f4e
Gianpaolo <pao.barga@gmail.com>
2020-12-14 16:47:54 +0100

    final commit: refactored the code and added some rules

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-// Dati raccolti dall'utente
-var km = parseInt(prompt('Quanti km vuoi percorrere?'));
-var age = parseInt(prompt('Quanti anni hai?'));
+// Dati raccolti dall'utente e verifica
+var km = prompt('Quanti km vuoi percorrere?');
+var age = "";
 
-// Costo in base ai km percorsi 
-var cost = km * 0.21;
-
-// Costo aggiornato in base all'età
-if (age < 18) {
-    cost = cost - (cost / 100 * 20);
-} else if (age > 65) {
-    cost = cost - (cost / 100 * 40);
+if (isNaN(km)) {
+    alert("Il dato inserito non è un numero, si prega di fare il refresh della pagina e di inserire solo dati numerici")
 } else {
-    null;
+    age = prompt('Quanti anni hai?');
+    if (isNaN(age)) {
+        alert("Il dato inserito non è un numero, si prega di fare il refresh della pagina e di inserire solo dati numerici")
+    } else {
+        // Conversione dati utente in numeri
+        km = parseInt(km);
+        age = parseInt(age);
+
+        // Costo in base ai km percorsi 
+        var cost = km * 0.21;
+
+        // Costo aggiornato in base all'età
+        if (age < 18) {
+            cost = cost - (cost / 100 * 20);
+        } else if (age > 65) {
+            cost = cost - (cost / 100 * 40);
+        } else {
+            null;
+        }
+
+        // Costo arrotondato ai centesimi
+        cost = Math.round(cost);
+
+        document.querySelector("#cost").innerHTML = cost;
+    }
 }
-
-// Costo arrotondato ai centesimi
-cost = Math.round(cost);
-
-document.querySelector("#cost").innerHTML = cost;
